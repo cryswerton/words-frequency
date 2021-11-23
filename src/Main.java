4918df4d1af85c2ed816896195741b22b6bf29b0
cryswerton <cryswertons@gmail.com>
2021-11-23 15:04:49 -0300

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -11,90 +11,16 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		String filePath;
-		filePath = "C:\\Users\\crysw\\Documents\\bm-subs\\bvm-subs.srt";
-		//filePath = "C:\\Users\\crysw\\Documents\\notes2.txt";
-		ArrayList<Word> words;
+		String regexPattern = "[a-zA-Z]+'?[a-zA-Z]+";
 		
-		File file = new File(filePath);
-		Scanner scan = new Scanner(file, "UTF-8");
+		//filePath = "C:\\Users\\crysw\\Documents\\bm-subs\\bvm-subs.srt";
+		filePath = "C:\\Users\\crysw\\Documents\\test.txt"; // change it to you're desired path.
+		WordsContent wdct = new WordsContent(filePath, regexPattern);
+		wdct.createListOfWords();
+		wdct.createWordsFrequency();
+		wdct.sortWords(true);
+		wdct.printListOfWords();
 		
-		String fileContent = "";
-		while(scan.hasNextLine()) {
-			fileContent = fileContent.concat(scan.nextLine() + "\n");
-		}
-		
-//		System.out.println(fileContent);
-//		System.out.println(matchFound("\\w+", fileContent));
-		
-		// Regex to get only words
-		words = getWords("[a-zA-Z]+'?[a-zA-Z]+", fileContent);
-		words = updateWordsFrequency(words);
-		sortWords(true, words);
-		
-		printWords(words);
-		
-//		for(Word word: words) {
-//			System.out.printf("%s; frequency: %d\n", word.getName(), word.getFrequency());
-//		}
-		
-	}
-	
-	// Returns an array list with all the matches
-	static ArrayList<Word> getWords(String patternText, String textContent){
-		String s = textContent;
-		Matcher m = Pattern.compile(patternText).matcher(s);
-		ArrayList<Integer> positions = new ArrayList<Integer>();
-		ArrayList<Word> words = new ArrayList<Word>();
-		while (m.find())
-		{
-		    positions.add(m.start());
-		    words.add(new Word(m.group()));
-		}
-		//System.out.println(positions);
-		
-		return words;
-	}
-	
-	static int getWordFrequency(String word, ArrayList<Word> words) {
-		int count = 0;
-		for(Word wd: words) {
-			if(wd.getName().equals(word)) {
-				count++;
-			}
-		}
-		return count;
-	}
-	
-	static ArrayList<Word> updateWordsFrequency(ArrayList<Word> words) {
-		for(Word word: words) {
-			word.setFrequency(getWordFrequency(word.getName(), words));
-		}
-		return words;
-	}
-	
-	static void sortWords(boolean option, ArrayList<Word> words) {
-		// true = descending; false ascending.
-		if(option) {
-			words.sort(Comparator.comparing(Word::getFrequency).reversed());
-		}else {
-			words.sort(Comparator.comparing(Word::getFrequency));
-		}
-	}
-	
-	static void printWords(ArrayList<Word> words) {
-		ArrayList<Word> uniqueWords = new ArrayList<Word>();
-		ArrayList<String> wordsAlreadyAdded = new ArrayList<String>();
-		int currentWordFrequency = 0;
-		for(Word word: words) {
-			if(!wordsAlreadyAdded.contains(word.getName())) {
-				uniqueWords.add(word);
-				wordsAlreadyAdded.add(word.getName());				
-			}
-		}
-		
-		for(Word word: uniqueWords) {
-			System.out.printf("%s; frequency: %d\n", word.getName(), word.getFrequency());
-		}
 	}
 
 }
---
+++ src/WordsContent.java
@@ -0,0 +1,92 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WordsContent {
+	
+	private String filePath;
+	private File file;
+	private String fileContent = "";
+	private String regexPattern;
+	private Scanner scan;
+	private ArrayList<Word> words;
+	
+	public WordsContent(String filePath, String regexPattern) throws FileNotFoundException {
+		super();
+		this.filePath = filePath;
+		this.file = new File(filePath);
+		this.regexPattern = regexPattern;
+		this.scan = new Scanner(file, "UTF-8");
+		this.fileContent = getFileContent();
+		this.words = new ArrayList<Word>();
+	}
+	
+	public void createListOfWords(){
+		Matcher matcher = Pattern.compile(this.regexPattern).matcher(this.fileContent);
+		while (matcher.find())
+		{
+			// get the group of words matched by the regex and assign each one to the ArrayList words.
+			this.words.add(new Word(matcher.group()));
+		}		
+	}
+	
+	public void createWordsFrequency() {
+		for(Word word: this.words) {
+			word.setFrequency(this.getWordFrequency(word.getName()));
+		}
+	}
+	
+	public void sortWords(boolean option) {
+		// true = descending; false ascending.
+		if(option) {
+			words.sort(Comparator.comparing(Word::getFrequency).reversed());
+		}else {
+			words.sort(Comparator.comparing(Word::getFrequency));
+		}
+	}
+	
+	public void printListOfWords() {
+		for(Word word: getUniqueListOfWords()) {
+			System.out.printf("%s; frequency: %d\n", word.getName(), word.getFrequency());
+		}
+	}
+	
+	private String getFileContent() {
+		while(this.scan.hasNextLine()) {
+			this.fileContent = this.fileContent.concat(scan.nextLine() + "\n");
+		}
+		return this.fileContent;
+	}
+	
+	private int getWordFrequency(String word) {
+		int count = 0;
+		for(Word wd: this.words) {
+			if(wd.getName().equals(word)) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	private ArrayList<Word> getUniqueListOfWords() {
+		ArrayList<Word> uniqueWords = new ArrayList<Word>();
+		ArrayList<String> wordsAlreadyAdded = new ArrayList<String>();
+		int currentWordFrequency = 0;
+		for(Word word: this.words) {
+			if(!wordsAlreadyAdded.contains(word.getName())) {
+				uniqueWords.add(word);
+				wordsAlreadyAdded.add(word.getName());				
+			}
+		}
+		return uniqueWords;
+	}
+	
+	
+	
+	
+
+}
